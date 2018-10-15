c209cae4edffe76d98ee0928e86ae0f359883bd2
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-15 20:24:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,9 +13,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      
+
     elsif req.path.match(/cart/)
-      
+      if @@cart == []
+        resp.write "Your cart is empty"
     else
       resp.write "Path Not Found"
     end
